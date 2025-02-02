2f7792d99228be14ba80b23edff3e439d406b8b7
Ywxig <82500877+Ywxig@users.noreply.github.com>
2025-02-02 21:54:25 +0200

    Add files via upload

---
+++ PageLoader.js
@@ -0,0 +1,107 @@
+
+__DEBUG__ = true
+player_pos = [0, 0]
+block_size = 32
+map = createMatrix(30, 30, defaultValue = '0');
+
+function printd(value, debugMode = True) {
+    if (debugMode == true) {
+        console.log(value)
+    }
+}
+
+function loadPage(page) {
+    
+    const content = document.getElementById('content');
+    switch (page) {
+      case 'home':
+        content.innerHTML = mainPage();
+        printd("[Home]", __DEBUG__);
+        break;
+        
+        case 'game':
+            content.innerHTML = gamePage();
+            printd("[game]", __DEBUG__);
+            break;
+
+      default:
+        printd("Fail :(", __DEBUG__);
+        content.innerHTML = `<h1>Страница не найдена</h1>`;
+    }
+  }
+
+function mainPage() {
+    let page = `
+    <h1>Главная страница</h1>
+    `
+    return page;
+}
+
+function consoleMapRender(map, player_pos) {
+    map[player_pos[1]][player_pos[0]] = "@";
+    let arr = [];
+    for (let x = 0; x < map.length; x++) {
+        arr.push(map[x].join(""))
+    }
+    console.log(arr.join("\n"))
+    map[player_pos[1]][player_pos[0]] = "0";
+}
+
+function canvasMapRender(map, player_pos) {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
+            if (map[i][j] == "0") { // если блок это пустое место то отрисовывается соответствующая текстура
+                ctx.fillStyle = "blue";
+                ctx.fillRect(block_size * i, block_size * j, block_size, block_size)                
+            }
+            
+        }
+        ctx.fillStyle = "pink";
+        ctx.fillRect(block_size * player_pos[0], block_size * player_pos[1], block_size, block_size)   
+    }
+}
+
+function createMatrix(M, N, defaultValue = 0) {
+    const matrix = [];
+    
+    for (let i = 0; i < M; i++) {
+        const row = [];
+        for (let j = 0; j < N; j++) {
+            row.push(defaultValue);  // Можно изменить defaultValue на любое значение
+        }
+        matrix.push(row);
+    }
+
+    return matrix;
+}
+
+// Пример использования
+const matrix = createMatrix(3, 4);  // Создаст матрицу 3x4, заполненную нулями
+console.log(matrix);
+
+function move(direction) {
+    if (direction == "up") {
+        player_pos[1]--;
+    }
+    if (direction == "down") {
+        player_pos[1]++;
+    }
+    if (direction == "right") {
+        player_pos[0]++;
+    }
+    if (direction == "left") {
+        player_pos[0]--;
+    }
+    canvasMapRender(map, player_pos);
+}
+
+function gamePage() {
+    const content = document.getElementById('content');
+    while (game = true) {
+        printd("[game stap]", __DEBUG__);
+        printd("", __DEBUG__);
+        content.innerHTML = mainPage();
+    }
+}
